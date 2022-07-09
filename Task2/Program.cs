0d2106255709e044e3b691cfe169a51b07a27402
Каверин Руслан Андреевич <ruslan.kaverin@dkc.ru>
2022-07-09 22:37:44 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+int result = x + y;
+Console.WriteLine(result);
