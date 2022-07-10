37d1de22fa1b6b77cb14ade44e074ebbc7dc62e3
Каверин Руслан Андреевич <ruslan.kaverin@dkc.ru>
2022-07-10 17:39:48 +0300

    Task16

---
+++ Task16/Program16.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Какие есть четные числа до заданного начиная от 1?");
+Console.WriteLine();
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = 1;
+Console.WriteLine(y);
+while (y <= x)
+{
+    
+    if(y % 2 == 0)
+    Console.WriteLine(y);
+    y += 1;
+}
