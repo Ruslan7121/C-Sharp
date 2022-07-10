e96c297d6831146835e803ff21edfd696836c16e
Каверин Руслан Андреевич <ruslan.kaverin@dkc.ru>
2022-07-10 17:03:46 +0300

    Task9

---
+++ Task9/Program9.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите меньшее число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите большее число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if(x*x == y)
+{
+    Console.Write("Да");
+}
+else 
+{
+    Console.Write("Нет");
+}
