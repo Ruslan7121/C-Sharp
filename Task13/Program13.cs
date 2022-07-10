ed6fbf1ebadc7e81f84f0e2852a324f1c3190cd3
Каверин Руслан Андреевич <ruslan.kaverin@dkc.ru>
2022-07-10 17:32:32 +0300

    Task13

---
+++ Task13/Program13.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Какое из двух чисел больше?");
+Console.WriteLine();
+Console.Write("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if(x > y)
+{
+    Console.Write("max = " );
+    Console.Write(x);
+}
+else 
+{
+    Console.Write("max = ");
+    Console.Write(y);
+}
