42679a1bd6c919fde2d1a43f49bd4e3cc5e37e60
Каверин Руслан Андреевич <ruslan.kaverin@dkc.ru>
2022-07-10 17:17:38 +0300

    Task10

---
+++ Task10/Program10.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (x == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (x == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (x == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (x == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (x == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (x == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+if (x > 7)
+{
+    Console.WriteLine("Введите число от 1 до 7");
+}
