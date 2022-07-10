b2b710b6a59c4f7c96b0cab10a68413bd041016b
Каверин Руслан Андреевич <ruslan.kaverin@dkc.ru>
2022-07-10 17:36:31 +0300

    Task15

---
+++ Task15/Program15.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Делится ли число на 2?");
+Console.WriteLine();
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if(x % 2 == 0)
+{
+    Console.Write("Да");
+}
+else 
+{
+    Console.Write("Нет");
+}
