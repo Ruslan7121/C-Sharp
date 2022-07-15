ef9c97d32cf72fe5f5969a9798b5c4570e20c411
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-07-15 13:22:34 +0300

    T20

---
+++ Task20/Program20.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int Number) {
+  if (Number == 6 | Number == 7)   // | - опреатор "или"
+  {
+  Console.WriteLine("Это выходной день!");
+  }
+
+  else 
+  if (Number < 1 | Number > 7) 
+  {
+    Console.WriteLine("Это не день недели!");
+  }
+  else Console.WriteLine("Это не выходной день!");
+}
+
+CheckingTheDayOfTheWeek(Number);
