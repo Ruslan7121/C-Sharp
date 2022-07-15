c0b0bbe19de40da2cae3fe1f53ff1b1323fbc045
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-07-15 13:07:50 +0300

    T19

---
+++ Task19/Program19.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введи трехзначное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+string Number = Convert.ToString(x);
+if (Number.Length > 2)
+{
+  Console.WriteLine("Третья цифра: " + Number[2]);
+}
+else 
+{
+  Console.WriteLine("Третьей цифры нет!");
+}
