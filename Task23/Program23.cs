4b1848aa91defa0868bc9f45f0be220bd9fda7ba
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-07-31 15:19:41 +0300

    T23

---
+++ Task23/Program23.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int aa = Convert.ToInt32(Console.ReadLine());
+void Coub(int a)
+{
+    for (int i = 1; i <= a; i++)
+    {
+        Console.Write(i * i * i + " ");
+    }
+}
+Coub(aa);
