ddde6d4a5ab3accb6484cc1fe444cbb040159f3f
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-01 20:13:23 +0300

    T25

---
+++ Task25/Program25.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ночер четверти: ");
+int xx = Convert.ToInt32(Console.ReadLine());
+string check_x(int x)
+{
+    if (x == 1) return ("Первая четверть");
+    else if (x == 2) return ("Вторая четверть");
+    else if (x == 3) return ("Третья четверть");
+    else if (x == 4) return ("Четвертая четверть");
+    else return ("Такой четверти нет");
+}
+Console.WriteLine(check_x(xx));
