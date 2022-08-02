48cebee64b141b40ad716431f07cb860b36817ae
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-02 22:05:29 +0300

    T30

---
+++ Task30/Program30.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int s = Convert.ToInt32(Console.ReadLine());
+int num (int s)
+    {
+    int count = 0;
+    if (s == 0) count = 1;
+    while(s != 0)
+
+        {
+            s = s / 10;
+            count = count + 1;
+            
+        }
+    return count; 
+    }       
+Console.Write($"Цифр в числе: {num(s)}");
