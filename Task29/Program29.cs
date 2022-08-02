8858aa41a6c02c5cb6f21c587396146f183ddb71
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-02 22:00:28 +0300

    T29

---
+++ Task29/Program29.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введине число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int summ = 0;
+
+for (int i = 1; i <= x; i++)
+{
+    summ = summ + i;
+}
+Console.WriteLine($"Сумма чисел: {summ}");
