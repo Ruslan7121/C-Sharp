f2571a7b5e7dcda2069307b43d22249a5d4d2583
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-05 19:01:52 +0300

    T33

---
+++ Task33/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число степени: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation(int a, int b)   
+{
+  int result = 1;                    
+  for(int i=1; i <= b; i++)         
+  {
+    result = a * result;          
+  }
+    return result;                
+}
+  int exponentiation = Exponentiation(a, b);
+Console.Write($"Ответ: {exponentiation}");
