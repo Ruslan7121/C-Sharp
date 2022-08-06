7910c941ca7de75bd86358f6d36df3dbba540d90
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-06 20:49:28 +0300

    T37

---
+++ Task37/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите колличество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] massive = new int[size];
+
+for (int i = 0; i < massive.Length; i++)
+massive[i] = new Random().Next(-10, 10);
+
+Console.WriteLine("[{0}]", string.Join(", ", massive));
