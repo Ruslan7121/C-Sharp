fd7ba4e32c5f9c3171daf9694b214000a050efee
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-06 17:42:01 +0300

    T34

---
+++ Task34/Program34.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+for (int i = n; i > 0; i = i / 10)
+{
+    sum = sum + i % 10;
+}
+Console.Write($"Сумма цифр в числе: {sum}");
