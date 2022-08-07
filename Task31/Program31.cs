a7f6b4fdda88ee667a9fd1d27d4adfd95a912dff
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-07 16:53:27 +0300

    T31

---
+++ Task31/Program31.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] factor(int a)
+{
+
+    int[] myArray = new int[a];
+    int b = 1;
+    for (int i = 1; i <= a; i++)
+    {
+        b = b * i;
+        myArray[i - 1] = b;
+    }
+    return myArray;
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+PrintArray(factor(a));
