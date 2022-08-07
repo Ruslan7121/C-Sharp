8e6babed9a680a6cd025c42869a9ac9888eab225
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-07 17:03:41 +0300

    T36

---
+++ Task36/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите число: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] massive = new int[size];
+int sumN = 0;
+int sumP = 0;
+
+for (int i = 0; i < massive.Length; i++)
+{
+    massive[i] = new Random().Next(-9, 10);
+
+    if (massive[i] > 0)
+    {
+        sumP = sumP + massive[i];
+    }
+
+    else sumN = sumN + massive[i];
+
+
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+PrintArray(massive);
+Console.WriteLine($"\nСумма положительных чисел: {sumP}");
+Console.WriteLine($"Сумма отрицательных чисел чисел: {sumN}");
