c60a91f75b7446ee43a49b3f903eae767849a289
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-09 20:35:49 +0300

    T46

---
+++ Task46/Program.cs
@@ -0,0 +1,35 @@
+﻿void PrintArray(int[] col)
+{
+    Console.Write("[");
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+    Console.Write("]");
+}
+
+Console.Write("Введите размерность массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[] massive = new int[size];
+
+for (int i = 0; i < massive.Length; i++)
+{
+    massive[i] = new Random().Next(0, 5);
+}
+
+PrintArray(massive);
+Console.WriteLine();
+int count = size / 2;
+int bath = 0;
+for (int i = 0; i < count; i++)
+{
+    bath = massive[i];
+    massive[i] = massive[size - 1 - i];
+    massive[size - 1 - i] = bath;
+}
+
+PrintArray(massive);
