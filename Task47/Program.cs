600478d9378288dced04f228b022dbe782aee54e
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-09 20:57:53 +0300

    T47

---
+++ Task47/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = Coordinate("a");
+int b = Coordinate("b");
+int c = Coordinate("c");
+
+int Coordinate(string coorName)
+{
+    Console.Write($"Введите координату {coorName}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+if (a < (b + c) && b < (a + c) && c < (a + b)) Console.WriteLine("Да, это треугольник!");
+else Console.WriteLine("Нет, это не треугольник!");
