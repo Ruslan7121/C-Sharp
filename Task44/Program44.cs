aa7c65593e0989d5647bfaf8464f4c629a269266
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-09 19:54:00 +0300

    T44

---
+++ Task44/Program44.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите размерность массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[] massive = new int[size];
+int sum = 0;
+int count = 0;
+
+for (int i = 0; i < massive.Length; i++)
+{
+    massive[i] = new Random().Next(0, 5);
+    if (i % 2 != 0)
+    {
+        sum = sum + massive[i];
+        count++;
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    Console.Write("[");
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+    Console.Write("]");
+}
+
+PrintArray(massive);
+Console.WriteLine();
+Console.WriteLine($"\nСумма элементов на нечётных позициях: {sum}");
