e13ffd2de53c7d5a00b91509915e9d7f01c81e3a
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-09 21:04:30 +0300

    T48

---
+++ Task48/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите десятичное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string res = string.Empty;
+
+while(num > 0)
+{
+    res = (num % 2) + res;
+    num = num/2;
+}
+
+Console.WriteLine($"Двоичное число: {res}");
