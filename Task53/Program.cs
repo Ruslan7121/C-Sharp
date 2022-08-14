dfd2d56fe7caacb93f6afc026483d21d5007551f
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-14 15:03:37 +0300

    T53

---
+++ Task53/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите коллиечество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите колличество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [ , ] array = new int [m,n]; 
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = i+j;
+        }
+    }
+} 
+
+FillArray(array);
+PrintArray(array);
