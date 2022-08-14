b230ec6dc39e4de6dabed58c7e13975582273c80
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-14 17:18:52 +0300

    T56

---
+++ Task56/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите коллиечество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите колличество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m, n];
+Random random = new Random();
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write("{0,7:F2}", array[i, j]);    //0,7 - длина отступа столбцов, F2 - 2 два знака после запятой в полученных случайных элементах
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = random.NextDouble() * 10;
+        }
+    }
+}
+FillArray(array);
+PrintArray(array);
