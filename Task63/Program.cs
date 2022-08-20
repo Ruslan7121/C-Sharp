0c7a56fee35bde47d61311550d0c9af322b1f168
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-20 20:32:00 +0300

    T63.2

--- Task63/Program.cs
+++ Task63/Program.cs
@@ -26,16 +26,17 @@ for (int i = 0; i < numbers.GetLength(0); i++)
 
 Console.WriteLine("\nСтрока с наименьшей суммой элементов №" + (IndexLine) + ". Сумма элементов в ней равна " + (minsum) + ".");
 
-void PrintArray(int[,] matr)
+void PrintArray(int[,] array)
 {
-    for (int i = 0; i < matr.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < matr.GetLength(1); j++)
+        Console.Write("| ");
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($"{matr[i, j]} ");
-
+            Console.Write(array[i, j] + " ");
         }
-        Console.WriteLine();
+        Console.Write("|");
+        Console.WriteLine("");
     }
 }
 
