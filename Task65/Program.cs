ca0a7f0436220ce05bb60d1114a472fe6c21f0f7
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-20 21:40:36 +0300

    T65

---
+++ Task65/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Введите размерность квадратного массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] sqareMatrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+{
+    sqareMatrix[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+}
+
+PrintArray(sqareMatrix);
+
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0,3}", array[i, j]);
+        }
+        Console.Write("|");
+        Console.WriteLine("");
+    }
+}
