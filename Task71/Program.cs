7c44fd3a536c6c0dd72e7c841f0df0f27301b666
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-08-23 21:00:08 +0300

    T71

---
+++ Task71/Program.cs
@@ -0,0 +1,16 @@
+﻿int M = InputInt("Введите первое натуральное число: ");
+int N = InputInt("Введите второе натуральное число: ");
+Console.WriteLine($"Сумма натуральных чисел от {M} до {N} равна: {SumOfNaturalNumbers(M, N)}");
+
+int SumOfNaturalNumbers(int M, int N)
+{
+    if (M == N) return N;
+    return N + SumOfNaturalNumbers(M, N - 1);
+}
+
+int InputInt(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
